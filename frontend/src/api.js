7babef25a11f39c71f639be116de53243d2eb1f4
Rohan Sharma <rohansharmaxyz45@gmail.com>
2026-06-15 18:38:18 +0530

    Fix API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
 
-    return config;
-});
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 api.interceptors.response.use(
     (response) => response,
