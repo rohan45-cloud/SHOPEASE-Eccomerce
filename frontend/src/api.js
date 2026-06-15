d9b97688cbc96c57ae3fa255e6640b5e54af6339
Rohan Sharma <rohansharmaxyz45@gmail.com>
2026-06-15 18:29:52 +0530

    Fixed axios interceptor error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,30 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+    baseURL: "/api",
+});
 
-api.interceptors.request.use((c) => {
-  const token = localStorage.getItem("token");
-  if (token) c.headers.Authorization = `Bearer ${token}`;
-  return c;
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 api.interceptors.response.use(
-  (r) => r,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-      window.location.href = "/login";
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error);
     }
-    return Promise.reject(err);
-  }
 );
 
 export default api;
